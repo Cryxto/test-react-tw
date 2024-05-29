24ade184d31778ca8dfced5e74cd3d5e84791ec1
Cryxto <bagasadvan123@gmail.com>
2024-05-29 16:35:21 +0700

    fresh

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+     <h1>H1</h1>
+     <h2>H2</h2>
+     <h3>H3</h3>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+import plugin from 'tailwindcss'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+      serif: ["Poppins", "sans-serif"],
+    },
+  },
+  plugins: [
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.2xl') },
+        'h2': { fontSize: theme('fontSize.xl') },
+        'h3': { fontSize: theme('fontSize.lg') },
+      })
+    })
+  ],
+}
+
